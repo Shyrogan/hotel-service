d63b188facc7b16379e3f50a124dbb384d8dbf0c
SEljaafari <78881640+SEljaafari@users.noreply.github.com>
2023-11-24 18:49:02 +0100

    -feat(OffreService) Génération d'offres selon les dates de sejours

--- src/main/java/fr/samyseb/hotelservice/repositories/ReservationRepository.java
+++ src/main/java/fr/samyseb/hotelservice/repositories/ReservationRepository.java
@@ -1,9 +1,11 @@
 package fr.samyseb.hotelservice.repositories;
 
+import fr.samyseb.hotelservice.entities.Chambre;
 import fr.samyseb.hotelservice.entities.Reservation;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
 
@@ -12,4 +14,7 @@ public interface ReservationRepository extends CrudRepository<Reservation, UUID>
 
     List<Reservation> findAllByChambreNumero(long chambre_numero);
 
+    List<Reservation> findByChambreNumeroAndChambreHotelId(long chambreNumero, UUID hotelId);
+
+    Reservation findTopByChambreAndFinBeforeOrderByFinDesc(Chambre chambre, LocalDate debut);
 }
