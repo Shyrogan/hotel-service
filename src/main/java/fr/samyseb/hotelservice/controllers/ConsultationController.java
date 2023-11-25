6b860c6fe156842a77cdd5f0c48070310c3b08ad
SEljaafari <78881640+SEljaafari@users.noreply.github.com>
2023-11-25 18:17:12 +0100

    feat(Consultation) ajout prise en compte des agences lors de l'utilisation du service

--- src/main/java/fr/samyseb/hotelservice/controllers/ConsultationController.java
+++ src/main/java/fr/samyseb/hotelservice/controllers/ConsultationController.java
@@ -4,13 +4,13 @@ import fr.samyseb.hotelservice.pojos.Offre;
 import fr.samyseb.hotelservice.services.OffreService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.nio.charset.StandardCharsets;
 import java.time.LocalDate;
+import java.util.Base64;
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/consultation")
@@ -23,8 +23,21 @@ public class ConsultationController {
     public List<Offre> getOffres(@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate debut,
                                  @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate fin,
                                  @RequestParam(required = false) Float prixMin,
-                                 @RequestParam(required = false) Float prixMax) {
-        return offreService.create(debut, fin, prixMin, prixMax);
+                                 @RequestParam(required = false) Float prixMax,
+                                 @RequestHeader("Authorization") String authHeader) {
+
+
+        String[] credentials = decodeBasicAuth(authHeader);
+        UUID agenceId = UUID.fromString(credentials[0]);
+        String agencePassword = credentials.length > 1 ? credentials[1] : "";
+
+        return offreService.create(debut, fin, prixMin, prixMax, agenceId, agencePassword);
     }
 
+    private String[] decodeBasicAuth(String authHeader) {
+        String base64Credentials = authHeader.substring("Basic ".length());
+        byte[] credDecoded = Base64.getDecoder().decode(base64Credentials);
+        String decodedCredentials = new String(credDecoded, StandardCharsets.UTF_8);
+        return decodedCredentials.split(":", 2);
+    }
 }
